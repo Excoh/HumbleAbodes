1060c6afc65d96507c3a87cbc3dc9ec47b7693f4
ani6gup <ani6gup@gmail.com>
2016-10-01 15:02:37 -0700

    Initial Project Structure

---
+++ Assets/VRTK/Examples/Resources/Scripts/Grabability.cs
@@ -0,0 +1,32 @@
+﻿namespace  VRTK.Examples
+{ 
+using UnityEngine;
+
+
+    public class Grabability : VRTK_InteractableObject
+    {
+        private Vector3 currentPos;
+        public override void StartUsing(GameObject currentUsingObject)
+        {
+            base.StartUsing(currentUsingObject);
+            currentUsingObject.GetComponent<VRTK_InteractTouch>().ForceTouch(this.gameObject);
+            //currentPos = transform.position;
+            //currentPos.y = currentPos.y + 3;
+            //currentUsingObject.transform.position = currentPos; 
+        }
+
+        public override void StopUsing(GameObject usingObject)
+        {
+            base.StopUsing(usingObject);
+            StopTouching(usingObject);
+        }
+        // Update is called once per frame
+
+        protected override void Start()
+        {
+            base.Start();
+        }
+
+    }
+
+}
---
+++ Assets/VRTK/Examples/Resources/Scripts/Whirlygig.cs
@@ -0,0 +1,36 @@
+﻿namespace VRTK.Examples
+{
+    using UnityEngine;
+
+    public class Whirlygig : VRTK_InteractableObject
+    {
+        float spinSpeed = 0f;
+        Transform rotator;
+         
+        public override void StartUsing(GameObject usingObject)
+        {
+            base.StartUsing(usingObject);
+            Debug.Log("Using object name is " + usingObject.name);
+            spinSpeed = 360f;
+        }
+
+        public override void StopUsing(GameObject usingObject)
+        {
+            base.StopUsing(usingObject);
+            spinSpeed = 0f;
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+            rotator = transform.Find("Capsule");
+
+        }
+
+        protected override void Update()
+        {
+            rotator.transform.Rotate(new Vector3(spinSpeed * Time.deltaTime, 0f, 0f));
+
+        }
+    }
+}
